2df6261f399a6afb991c1768a2d265d672618bb7
Rajiur Rahman <rajiurrahman.bd@gmail.com>
2023-09-11 16:19:33 -0400

    Add files via upload

---
+++ UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace TestJwtApp1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        //[HttpGet("Public")]
+        [HttpGet]
+        [Route("PublicEndpoint")]
+        public IActionResult Public()
+        {
+
+            return Ok("Hello World! Inside public endpoint.");
+        }
+
+        [HttpGet("Admin")]
+        [Authorize(Roles ="Admin")]
+        public IActionResult AdminsEndpoint()
+        {
+            var userClaims = HttpContext.User.Identity as ClaimsIdentity;
+            var currentUser = AuthenticationHelper.GetCurrentUser(userClaims);
+
+            return Ok($"Hi! {currentUser.GivenName}, you are a {currentUser.Role}");
+        }
+
+        [HttpGet("Seller")]
+        [Authorize(Roles = "Seller")]
+        public IActionResult SellerssEndpoint()
+        {
+            var userClaims = HttpContext.User.Identity as ClaimsIdentity;
+            var currentUser = AuthenticationHelper.GetCurrentUser(userClaims);
+
+            return Ok($"Hi! {currentUser.GivenName}, you are a {currentUser.Role}");
+        }
+
+
+    }
+}
